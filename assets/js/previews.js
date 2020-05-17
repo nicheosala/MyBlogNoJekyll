34dbdf1ec69c72f4e29e4c74518f31fb73e1b946
Nicolò <48255219+nicheosala@users.noreply.github.com>
2020-05-17 16:11:07 +0200

    Now the article previews are loaded from a json file. (!)

--- assets/js/previews.js
+++ assets/js/previews.js
@@ -1,26 +1,14 @@
-let text = `
-[
-    {
-      "title": "Ottimizzare Windows 10",
-      "summary": "Windows è pieno di funzionalità obsolete, inutili, alcune nocive. Rendiamolo migliore.",
-      "date": "1 maggio 2020",
-      "url": "/pages/00001.html"
-    },
-    {
-      "title": "Article title",
-      "summary": "Article summary",
-      "date": "Article date",
-      "url": "/pages/#"
-    }
-]
-`
-
-let json = JSON.parse(text);
 let previews = document
     .querySelector("main")
     .appendChild(document.createElement("ul"));
 
-json.forEach(item => {
+fetch("assets/json/previews.json")
+    .then(response => response.json())
+    .then(json => json.forEach(
+        item => toHTML(item)
+    ));
+
+function toHTML(item) {
     let preview = document.createElement("li");
     let {title, summary, date, url} = item;
     preview.innerHTML = `
@@ -31,19 +19,4 @@ json.forEach(item => {
     </a>
     `
     previews.appendChild(preview);
-    });
-
-/*
-for (let i = 0; i < json.length; i++) {
-
-    let preview = document.createElement("li");
-    let {title, summary, date, url} = json[i];
-    preview.innerHTML = \`
-    <a class="preview" href=${url}>
-        <h2 class="title">${title}</h2>
-        <p class="summary">${summary}</p>
-        <p class="date">${date}</p>
-    </a>
-    \`
-    previews.appendChild(preview);
-}*/
+}
