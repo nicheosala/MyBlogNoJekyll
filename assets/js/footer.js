8ccbe2685a7df9101ca082aa7fc85e29a72c7c68
Nicolò <48255219+nicheosala@users.noreply.github.com>
2020-05-17 11:17:08 +0200

    Minor style changes ++ style.css import restored into the head section.

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,13 +1,13 @@
 document.getElementById("footer-placeholder").innerHTML = `
 <nav class="donazioni">
-    <p><strong>Donazioni</strong></p>
+    <h4><strong>Donazioni</strong></h4>
     <ul>
         <li><a href="https://paypal.me/naicols" target="_blank" rel="noreferrer">PayPal</a></li>
         <li><a href="#" target="_blank" rel="noreferrer">Bitcoin</a></li>
     </ul>
 </nav>
 <nav class="social">
-    <p><strong>Contatti</strong></p>
+    <h4><strong>Contatti</strong></h4>
     <ul>
         <li><a href="https://github.com/nicheosala" target="_blank" rel="noreferrer">Github</a></li>
         <li><a href="https://instagram.com/nicheosala" target="_blank" rel="noreferrer">Instagram</a></li>
