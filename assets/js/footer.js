f997e68b18b99fcb4c38bf0d5fcdc082ac7c576d
Nicolò <48255219+nicheosala@users.noreply.github.com>
2020-06-02 13:13:57 +0200

    Some minor changes in order to make the website fully-acessible, w.r.t. Google Lighthouse and WAVE metrics.

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,13 +1,13 @@
 document.querySelector("footer").innerHTML = `
 <nav class="donazioni">
-    <h4><strong>Donazioni</strong></h4>
+    <p><strong>Donazioni</strong></p>
     <ul>
         <li><a href="https://paypal.me/naicols" target="_blank" rel="noreferrer">PayPal</a></li>
         <li><a href="#" target="_blank" rel="noreferrer">Bitcoin</a></li>
     </ul>
 </nav>
 <nav class="social">
-    <h4><strong>Contatti</strong></h4>
+    <p><strong>Contatti</strong></p>
     <ul>
         <li><a href="https://github.com/nicheosala" target="_blank" rel="noreferrer">Github</a></li>
         <li><a href="https://instagram.com/nicheosala" target="_blank" rel="noreferrer">Instagram</a></li>
